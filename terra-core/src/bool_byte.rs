8918e6d97bc07648bc22ec86efb1f4fbb11e569f
RLGingerBiscuit <29359076+RLGingerBiscuit@users.noreply.github.com>
2022-12-04 17:17:41 +0000

    Add BoolByte (a.k.a BitsByte)

---
+++ terra-core/src/bool_byte.rs
@@ -0,0 +1,79 @@
+use bit_reverse::ParallelReverse;
+
+#[derive(thiserror::Error, Debug)]
+pub enum BoolByteError {
+    #[error("Invalid index: {0}.")]
+    InvalidIndex(u8),
+}
+
+#[derive(Default, Debug)]
+pub struct BoolByte {
+    value: u8,
+}
+
+impl From<u8> for BoolByte {
+    fn from(value: u8) -> Self {
+        Self { value }
+    }
+}
+
+impl From<BoolByte> for u8 {
+    fn from(bb: BoolByte) -> Self {
+        bb.value
+    }
+}
+
+impl BoolByte {
+    fn check_index(&self, index: u8) -> Result<(), BoolByteError> {
+        if index >= 8 {
+            Err(BoolByteError::InvalidIndex(index).into())
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn clear_all(&mut self) {
+        self.value = u8::MIN
+    }
+
+    pub fn set_all(&mut self) {
+        self.value = u8::MAX
+    }
+
+    pub fn get(&self, index: u8) -> Result<bool, BoolByteError> {
+        self.check_index(index)?;
+        Ok(self.value & (1 << index) != 0)
+    }
+
+    pub fn on(&mut self, index: u8) -> Result<(), BoolByteError> {
+        self.check_index(index)?;
+        self.value |= 1 << index;
+        Ok(())
+    }
+
+    pub fn off(&mut self, index: u8) -> Result<(), BoolByteError> {
+        self.check_index(index)?;
+        self.value &= (1 << index).swap_bits();
+        Ok(())
+    }
+
+    pub fn set(&mut self, index: u8, value: bool) -> Result<(), BoolByteError> {
+        if value {
+            self.on(index)
+        } else {
+            self.off(index)
+        }
+    }
+
+    pub fn toggle(&mut self, index: u8) -> Result<(), BoolByteError> {
+        if self.get(index)? {
+            self.off(index)
+        } else {
+            self.on(index)
+        }
+    }
+
+    pub fn set_value(&mut self, value: u8) {
+        self.value = value
+    }
+}
