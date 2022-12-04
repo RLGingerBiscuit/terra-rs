7f7ceec5668f2945ed9e4e7ea9340069be403c19
RLGingerBiscuit <29359076+RLGingerBiscuit@users.noreply.github.com>
2022-12-04 22:54:47 +0000

    As far as I can get without implementing loadouts

--- terra-core/src/player.rs
+++ terra-core/src/player.rs
@@ -1,11 +1,15 @@
-use std::{path::PathBuf, str::FromStr};
+use std::{fs::File, io::ErrorKind, path::PathBuf, str::FromStr};
 
+use aesstream::AesReader;
 use anyhow::Result;
+use byteorder::{ReadBytesExt, LE};
+use crypto::aessafe::AesSafe128Decryptor;
 
 use crate::{
-    buff::Buff, difficulty::Difficulty, item::Item, prefix::Prefix, Color, AMMO_COUNT, BANK_COUNT,
-    BUFF_COUNT, BUILDER_ACCESSORY_COUNT, CELLPHONE_INFO_COUNT, COINS_COUNT, CURRENT_VERSION,
-    DPAD_BINDINGS_COUNT, EQUIPMENT_COUNT, INVENTORY_COUNT, LOADOUT_COUNT, TEMPORARY_SLOT_COUNT,
+    buff::Buff, difficulty::Difficulty, io_extensions::TerraReadExt, item::Item, prefix::Prefix,
+    Color, AMMO_COUNT, BANK_COUNT, BUFF_COUNT, BUILDER_ACCESSORY_COUNT, CELLPHONE_INFO_COUNT,
+    COINS_COUNT, CURRENT_VERSION, DPAD_BINDINGS_COUNT, ENCRYPTION_BYTES, EQUIPMENT_COUNT,
+    INVENTORY_COUNT, LOADOUT_COUNT, MAGIC_MASK, MAGIC_NUMBER, TEMPORARY_SLOT_COUNT,
     TICKS_PER_MICROSECOND,
 };
 
@@ -248,6 +252,79 @@ impl Player {
         items: &Vec<Item>,
         buffs: &Vec<Buff>,
     ) -> Result<()> {
+        let filepath_buf: PathBuf = filepath.into();
+        let filepath_string: String = filepath_buf.clone().into_os_string().into_string().unwrap();
+
+        let file = match File::open(&filepath_buf) {
+            Ok(f) => f,
+            Err(e) => {
+                return Err(match e.kind() {
+                    ErrorKind::NotFound => PlayerError::FileNotFound(filepath_string),
+                    ErrorKind::PermissionDenied => PlayerError::AccessDenied(filepath_string),
+                    _ => PlayerError::Failure(filepath_string),
+                }
+                .into())
+            }
+        };
+
+        let decryptor = AesSafe128Decryptor::new(ENCRYPTION_BYTES);
+        let mut reader = match AesReader::new_with_iv(file, decryptor, ENCRYPTION_BYTES) {
+            Ok(r) => r,
+            Err(_) => return Err(PlayerError::Failure(filepath_string).into()),
+        };
+
+        self.version = reader.read_i32::<LE>()?;
+
+        if self.version > CURRENT_VERSION {
+            return Err(PlayerError::PostDated(filepath_string, self.version).into());
+        }
+
+        if self.version >= 135 {
+            // The string "relogic", followed by a 1-byte filetype
+            let magic_num = reader.read_u64::<LE>()?;
+
+            // Both MAGIC_MASK and MAGIC_NUMBER were taken directly from Terraria's exe
+            if magic_num & MAGIC_MASK != MAGIC_NUMBER {
+                return Err(PlayerError::IncorrectFormat(filepath_string).into());
+            }
+
+            // The file type of the file
+            //   None = 0
+            //    Map = 1
+            //  World = 2
+            // Player = 3
+            if (magic_num >> 56) & 255 != 3 {
+                return Err(PlayerError::IncorrectFileType(filepath_string).into());
+            }
+
+            // This u32 is a 'revision' field, that is only used for type 1 files (Map)
+            self.revision = reader.read_u32::<LE>()?;
+            // This u64 is a 'favourited' field, which for Players, is handled by favourites.json
+            self.favourited = reader.read_u64::<LE>()?;
+        }
+
+        // This method mimics C#'s BinaryReader.ReadString(),
+        // prefixing the string with its length in ULEB128 format
+        self.name = reader.read_lpstring()?;
+
+        if self.version >= 10 {
+            self.difficulty = Difficulty::from(reader.read_u8()?);
+        } else if reader.read_bool()? {
+            self.difficulty = Difficulty::Hardcore;
+        }
+
+        if self.version >= 138 {
+            self.playtime = reader.read_i64::<LE>()?;
+        }
+
+        self.hair_style = reader.read_i32::<LE>()?;
+
+        if self.version >= 72 {
+            self.hair_dye = reader.read_u8()?;
+        }
+
+        
+
         todo!("Player.load()")
     }
     pub fn save(&self, filepath: impl Into<PathBuf>) -> Result<()> {
