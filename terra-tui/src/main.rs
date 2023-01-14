a54af7f26be1c2696d98610effc52061ef36d1d2
RLGingerBiscuit <29359076+RLGingerBiscuit@users.noreply.github.com>
2023-01-14 19:17:06 +0000

    Make terra-tui test the added characters

--- terra-tui/src/main.rs
+++ terra-tui/src/main.rs
@@ -1,15 +1,82 @@
-use std::{fs::OpenOptions, io::BufWriter};
+use std::{fs::File, io::Read, path::PathBuf};
 
 use anyhow::Result;
 
-use terra_core::{buff::Buff, item::Item, player::Player, prefix::Prefix, utils::get_player_dir};
+use terra_core::{buff::Buff, item::Item, player::Player, prefix::Prefix};
+
+#[derive(thiserror::Error, Debug)]
+pub enum TestError {
+    #[error("Error during loading")]
+    Load(anyhow::Error),
+    #[error("Error during saving")]
+    Save(anyhow::Error),
+    #[error("Error during comparison")]
+    Comparison,
+}
+
+const VERSIONS: [usize; 16] = [
+    39, 69, 73, 77, 93, 98, 145, 168, 175, 184, 190, 225, 230, 237, 248, 269,
+];
+
+fn run_test(
+    version: usize,
+    directory: &PathBuf,
+    items: &Vec<Item>,
+    buffs: &Vec<Buff>,
+    prefixes: &Vec<Prefix>,
+) -> Result<(), TestError> {
+    let chara_name = format!("v{}", version);
+    let filepath = directory.join(format!("{}.plr", &chara_name));
+
+    let mut plr = Player::default();
+
+    println!("Version {}", version);
+
+    if let Err(err) = plr.load(&filepath, prefixes, items, buffs) {
+        return Err(TestError::Load(err));
+    }
+
+    println!("\tName: {}", &plr.name);
+
+    let out_filepath = directory.join(format!("{}.saved.plr", &chara_name));
+
+    if let Err(err) = plr.save(&out_filepath) {
+        return Err(TestError::Save(err));
+    }
+
+    let mut old_file = File::open(&filepath).expect("Could not open old file");
+    let mut new_file = File::open(&out_filepath).expect("Could not open new file");
+
+    let mut old_buf = [0; 10000];
+    let mut new_buf = [0; 10000];
+
+    loop {
+        if let Ok(r1) = old_file.read(&mut old_buf) {
+            if r1 > 0 {
+                if let Ok(r2) = new_file.read(&mut new_buf) {
+                    if r1 == r2 {
+                        if old_buf == new_buf {
+                            continue;
+                        }
+                    }
+                    return Err(TestError::Comparison);
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    Ok(())
+}
 
 fn main() -> Result<()> {
-    let chara_name = "Pogo";
-
-    let player_dir = get_player_dir();
-
-    let filepath = player_dir.join(format!("{}.plr", chara_name));
+    let mut player_dir = PathBuf::new();
+    player_dir.push("./tests");
 
     let items = Item::load_items()?;
     let buffs = Buff::load_buffs()?;
@@ -19,33 +86,16 @@ fn main() -> Result<()> {
     println!("Buffs count: {}", buffs.len());
     println!("Prefixes count: {}", prefixes.len());
 
-    let mut plr = Player::default();
-
-    println!("Default Player Name: {}", plr.name);
-
-    plr.load(&filepath, &prefixes, &items, &buffs)?;
-
-    println!("Current Player Name: {}", plr.name);
-
-    // Serde Serialization
-    {
-        let mut json_file = OpenOptions::new()
-            .create(true)
-            .write(true)
-            .append(false)
-            .open(player_dir.join(format!("{}.json", chara_name)))?;
-
-        let mut json_writer = BufWriter::new(&mut json_file);
-
-        serde_json::to_writer_pretty(&mut json_writer, &plr)?;
+    for version in VERSIONS {
+        match run_test(version, &player_dir, &items, &buffs, &prefixes) {
+            Ok(_) => println!("v{} loaded/saved successfully", version),
+            Err(err) => match err {
+                TestError::Load(err) => println!("Error whilst loading: {}", err),
+                TestError::Save(err) => println!("Error whilst saving: {}", err),
+                TestError::Comparison => println!("Saved file was not the same as the loaded file"),
+            },
+        }
     }
-
-    let outpath = player_dir.join(format!("{}_The_Second.plr", chara_name));
-    plr.name = "Pogo The Second".to_string();
-
-    println!("New Player Name: {}", plr.name);
-
-    plr.save(&outpath)?;
 
     Ok(())
 }
