00860125de94141b9f56793fbeeb313029a424c6
RLGingerBiscuit <29359076+RLGingerBiscuit@users.noreply.github.com>
2023-04-06 15:44:13 +0100

    Add modal spacing and fix error closing

--- terra-gui/src/app/modals.rs
+++ terra-gui/src/app/modals.rs
@@ -13,18 +13,21 @@ impl App {
                 .fixed_size([360.0, 240.0])
                 .frame(egui::Frame::window(&ctx.style()).inner_margin(8.))
                 .show(ctx, |ui| {
-                    ui.spacing_mut().item_spacing.y = 8.0;
+                    ui.spacing_mut().item_spacing.y = 8.;
+
                     ui.vertical_centered(|ui| {
                         ui.heading("terra-rs");
                         ui.label("© 2023 RLGingerBiscuit - MIT");
                         ui.label(concat!("Version ", env!("CARGO_PKG_VERSION")));
                     });
-                    Grid::new("about_box").num_columns(2).show(ui, |ui| {
+
+                    Grid::new("about_grid").num_columns(2).show(ui, |ui| {
                         ui.label("Github:");
                         if ui.link(GITHUB_REPO_NAME).clicked() {
                             open::that(GITHUB_REPO_URL).ok();
                         }
                         ui.end_row();
+
                         ui.label("GUI Library:");
                         if ui.link("emilk/egui").clicked() {
                             open::that("https://github.com/emilk/egui").ok();
@@ -49,6 +52,8 @@ impl App {
                 .auto_sized()
                 .frame(Frame::window(&ctx.style()).inner_margin(8.))
                 .show(ctx, |ui| {
+                    ui.spacing_mut().item_spacing.y = 8.;
+
                     ui.label(err.to_string());
 
                     egui::CollapsingHeader::new("Details").show(ui, |ui| {
@@ -59,7 +64,7 @@ impl App {
 
                     ui.vertical_right_justified(|ui| {
                         if ui.button("Ok").clicked() {
-                            self.do_update(Message::CloseAbout);
+                            self.do_update(Message::CloseError);
                         }
                     });
                 });
